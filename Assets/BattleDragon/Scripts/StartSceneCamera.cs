d9da820f12db9305484f6f72f330ec20341ca931
GAMEABC\wangjiajun01 <Wangjiajun01@bianfeng.com>
2017-11-10 17:59:01 +0800

    11111

---
+++ Assets/BattleDragon/Scripts/StartSceneCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSceneCamera : MonoBehaviour {
+
+    public GameObject maincamera;
+    private double speed = 10;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        speed -= 0.01;
+        if (speed <= 0)
+        {
+            maincamera.SetActive(true);
+            GameObject.Find("MMCamera").SetActive(false);
+        }
+    }
+}
